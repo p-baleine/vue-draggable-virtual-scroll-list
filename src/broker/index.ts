fcb40393676442045bc0c9e4db298d259fda464e
p-baleine <p.baleine@gmail.com>
2020-04-19 06:30:16 +0900

    DI

--- src/broker/index.ts
+++ src/broker/index.ts
@@ -1,30 +1,72 @@
-import { CreateElement } from 'vue';
-import Draggable from 'vuedraggable'
-import VirtualList from 'vue-virtual-scroll-list'
-import { Vue } from 'vue-property-decorator'
+import { CreateElement, VueConstructor } from 'vue';
+import { Vue } from 'vue-property-decorator';
 
-import Policy from './policy'
+import Policy from './policy';
 
-interface DraggableEvent<T> extends Event {
-  moved?: {
-    oldIndex: number
-    newIndex: number
-  }
-  added?: {
-    element: T
-    newIndex: number
-  }
-  removed?: {
-    element: T
-    oldIndex: number
-  }
+export interface IDraggable<T> extends VueConstructor {
+  props: {
+    // Properties of vuedraggable
+    // See: https://github.com/SortableJS/Vue.Draggable#props
+    value: Array<T>;
+    clone: (x: T) => T;
+  };
+
+  $emit(event: 'change', e: DraggableEvent<T>): void;
+  $emit(event: keyof SortableEvents, e: Event): void;
 }
 
-export const draggableEvents = [
-  'moved', 'added', 'removed']
-export const sortableEvents = [
-  'start', 'add', 'remove', 'update', 'end', 'choose',
-  'unchoose', 'sort', 'filter', 'clone']
+export interface IVirtualList<T> extends VueConstructor {
+  props: {
+    // Properties of vue-virtual-scroll-list
+    // See: https://github.com/tangbc/vue-virtual-scroll-list#props-type
+    size?: number;
+    keeps: number;
+    dataKey: string;
+    dataSources: Array<T>;
+    dataComponent: Vue;
+  };
+}
+
+export enum SortableEvents {
+  start, add, remove, update, end,
+  choose, unchoose, sort, filter, clone,
+}
+
+export interface DraggableEvent<T> extends Event {
+  moved?: {
+    oldIndex: number;
+    newIndex: number;
+  };
+  added?: {
+    element: T;
+    newIndex: number;
+  };
+  removed?: {
+    element: T;
+    oldIndex: number;
+  };
+}
+
+const sortableEvents = Object.values(SortableEvents)
+  .filter(x => typeof x === 'string');
+const draggableEvents = ['moved', 'added', 'removed'];
+
+// Inherits VirtualList and overrides getRenderSlots.
+export default function createBroker<T>(
+  Draggable: IDraggable<T>,
+  VirtualList: IVirtualList<T>,
+  PolicyCtr: typeof Policy) {
+  return VirtualList.extend({
+    inject: {
+      Draggable: { from: 'Draggable', default: () => Draggable },
+      VirtualList: { from: 'VirtualList', default: () => VirtualList },
+      Policy: { from: 'Policy', default: () => PolicyCtr },
+    },
+    methods: {
+      getRenderSlots,
+    },
+  });
+}
 
 // This function will override VirtualList.options.methods.getRenderSlots.
 //
@@ -32,9 +74,10 @@ export const sortableEvents = [
 // which would be wrapped by Draggable.
 // Draggable's change events would be converted to input events and emitted.
 function getRenderSlots<T extends Record<string, T>>(h: CreateElement) {
-  const { getRenderSlots: original } = VirtualList.options.methods
-  const slots = original.call(this, h)
-  const policy = new Policy(this.dataKey, this.dataSources, this.range)
+  const { Draggable, VirtualList, Policy } = this;
+  const { getRenderSlots: original } = VirtualList.options.methods;
+  const slots = original.call(this, h);
+  const policy = new Policy(this.dataKey, this.dataSources, this.range);
 
   return [
     h(Draggable, {
@@ -53,22 +96,15 @@ function getRenderSlots<T extends Record<string, T>>(h: CreateElement) {
         // Propagate Sortable events.
         ...sortableEventHandlers(this),
       },
-      attrs: this.$attrs
-    }, slots)
-  ]
+      attrs: this.$attrs,
+    }, slots),
+  ];
 }
-
-// Inherits VirtualList and overrides getRenderSlots.
-export default VirtualList.extend({
-  methods: {
-    getRenderSlots
-  }
-})
 
 // Returns handlers which propagate sortable's events.
 export function sortableEventHandlers(context: Vue) {
   return sortableEvents.reduce((acc, eventName) => ({
     ...acc,
-    [eventName]: context.$emit.bind(context, eventName.toLowerCase())
-  }), {})
+    [eventName]: context.$emit.bind(context, eventName),
+  }), {});
 }
