2a2ca44953f50f5ac71d32e106741ec4d6893d64
tkow <t.kusama0613@outlook.jp>
2020-05-13 21:15:55 +0900

    [add] eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint"
+  ],
+  "plugins": [
+    "@typescript-eslint"
+  ],
+  "env": { "node": true, "es6": true },
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "sourceType": "module",
+    "project": "./tsconfig.json"
+  },
+  "rules": {
+  }
+}
